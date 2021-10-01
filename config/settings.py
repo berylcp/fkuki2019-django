de74c6a3b230a7a38726a2d0bbfe33cd52e45349
fkuki2019 <berylcp@gmail.com>
2021-10-01 07:49:51 +0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -124,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -143,3 +145,5 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+django_heroku.settings(config=locals(), staticfiles=False,logging=False)
